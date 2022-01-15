b0c8ccd0a43a6b835c27aca25411deb612ca7ed2
Lance Belarmino <lancebelarmino@gmail.com>
2022-01-15 15:15:48 +0800

    Add form validation and calculator logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,19 @@
 import '../scss/main.scss';
 
-const FormValidator = (function () {
+const formValidator = (function () {
+  let fields = [];
+
+  let isFieldValid = {
+    bill: null,
+    custom: true,
+    people: null,
+  };
+
   const regex = {
-    currency: /^[+-]?[1-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$/,
+    currency: /^([1-9]{1}[\d]{0,2}(\,[\d]{3})*(\.[\d]{0,2})?|[1-9]{1}[\d]{0,}(\.[\d]{0,2})?|0(\.[\d]{0,2})?|(\.[\d]{1,2})?)$/,
     percentage: /^(100(\.00?)?|[1-9]?\d(\.\d\d?)?)?%$/,
     population: /^[1-9]{1,3}((,[0-9]{3})*|([0-9])*)$/,
   };
-
-  let fields = [];
 
   const getFields = (selector) => {
     const fieldArr = Array.from(document.querySelectorAll(selector));
@@ -23,54 +29,57 @@ const FormValidator = (function () {
   };
 
   const validateField = (field) => {
-    if (field.value === '') {
-      setFieldStatus(field, {
-        isValid: false,
-        error: `Can't be blank`,
-      });
-    } else {
-      let error;
+    let error;
 
-      switch (field.id) {
-        case 'bill':
-          if (field.value == 0) {
-            error = `Can't be zero`;
-          }
-
+    switch (field.id) {
+      case 'bill':
+        if (field.value === '') {
           setFieldStatus(field, {
-            isValid: regex.currency.test(field.value),
-            error,
+            isValid: false,
+            error: `Can't be blank`,
           });
-          break;
+          return;
+        } else if (field.value == 0) {
+          error = `Can't be zero`;
+        }
 
-        case 'custom':
-          if (field.value == 0) {
-            error = `Can't be zero`;
-          } else if (field.value.includes('.')) {
-            error = `No decimals`;
-          } else if (field.value > 100) {
-            error = `< 100 only`;
-          }
+        setFieldStatus(field, {
+          isValid: regex.currency.test(field.value),
+          error,
+        });
+        break;
 
+      case 'custom':
+        if (field.value.includes('.')) {
+          error = `No decimals`;
+        } else if (field.value > 100) {
+          error = `< 100 only`;
+        }
+
+        setFieldStatus(field, {
+          isValid: regex.currency.test(field.value),
+          error,
+        });
+        break;
+
+      case 'people':
+        if (field.value === '') {
           setFieldStatus(field, {
-            isValid: regex.currency.test(field.value),
-            error,
+            isValid: false,
+            error: `Can't be blank`,
           });
-          break;
+          return;
+        } else if (field.value == 0) {
+          error = `Can't be zero`;
+        } else if (field.value.includes('.')) {
+          error = `No decimals`;
+        }
 
-        case 'people':
-          if (field.value == 0) {
-            error = `Can't be zero`;
-          } else if (field.value.includes('.')) {
-            error = `No decimals`;
-          }
-
-          setFieldStatus(field, {
-            isValid: regex.population.test(field.value),
-            error,
-          });
-          break;
-      }
+        setFieldStatus(field, {
+          isValid: regex.population.test(field.value),
+          error,
+        });
+        break;
     }
   };
 
@@ -84,14 +93,14 @@ const FormValidator = (function () {
       fieldMessage.classList.add('form__radio-message');
     }
 
-    console.log(status);
-
     if (status.isValid) {
       clearFieldStatus(field);
+      isFieldValid = { ...isFieldValid, [`${field.id}`]: true };
     } else {
       clearFieldStatus(field);
       fieldMessage.innerText = `${status.error}`;
       field.classList.add('form__input--error');
+      isFieldValid = { ...isFieldValid, [`${field.id}`]: false };
     }
 
     formBlock.insertBefore(fieldMessage, field.nextSibling);
@@ -108,22 +117,89 @@ const FormValidator = (function () {
   };
 
   return {
-    initialize(form, fieldSelector) {
+    init(form, fieldSelector) {
       getFields(fieldSelector);
       validateOnBlur();
+    },
+    getFormStatus() {
+      if (Object.values(isFieldValid).every((input) => input === true)) {
+        return true;
+      }
+      return false;
     },
   };
 })();
 
-const debounce = (func, delay) => {
-  let timerId;
-
-  return function () {
-    clearTimeout(timerId);
-    timerId = setTimeout(() => func.apply(this, arguments), delay);
-  };
-};
-
 const tipForm = document.querySelector('#tipForm');
 
-FormValidator.initialize(tipForm, '.form__input');
+formValidator.init(tipForm, '.js-formInput');
+
+const calculator = (function () {
+  let tip;
+
+  const calculatorHandler = () => {
+    const radio = document.querySelector('.js-formRadio');
+    const billInput = document.querySelector('#bill');
+    const peopleInput = document.querySelector('#people');
+    const radioInput = document.querySelector('.js-radioInput');
+
+    billInput.addEventListener('blur', () => {
+      getResult();
+    });
+
+    peopleInput.addEventListener('blur', () => {
+      getResult();
+    });
+
+    radio.addEventListener('click', (e) => {
+      if (e.target.classList.contains('form__radio-label')) {
+        tip = parseInt(e.target.dataset.amount);
+        getResult();
+      }
+    });
+
+    radioInput.addEventListener('blur', (e) => {
+      resetRadioBtn(e.target.value);
+      tip = parseInt(e.target.value);
+      getResult();
+    });
+  };
+
+  const resetRadioBtn = (enteredValue) => {
+    const radioBtnArr = document.querySelectorAll('.js-radioBtn');
+
+    if (enteredValue !== '') {
+      radioBtnArr.forEach((btn) => {
+        btn.checked = false;
+      });
+    }
+  };
+
+  const getResult = () => {
+    const isFormValid = formValidator.getFormStatus();
+
+    const bill = parseInt(document.querySelector('#bill').value);
+    const people = parseInt(document.querySelector('#people').value);
+
+    const tipPerPerson = (bill * tip) / 100 / people;
+    const billPerPerson = bill + tipPerPerson;
+
+    if (isFormValid) {
+      console.log(tipPerPerson, billPerPerson);
+      return {
+        tipPerPerson,
+        billPerPerson,
+      };
+    }
+  };
+
+  return {
+    init() {
+      calculatorHandler();
+    },
+    tipPerPerson: getResult.tipPerPerson,
+    billPerPerson: getResult.billPerPerson,
+  };
+})();
+
+calculator.init();
